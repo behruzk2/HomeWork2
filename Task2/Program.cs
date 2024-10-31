fef0f3a886e1b115ba98cbad00a5deb7cbdc9cb4
Behruz <behruzk2002@mail.ru>
2024-10-31 09:02:44 -0400

    Completed task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход координаты точки (X и Y),
+//причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, 
+//в которой находится эта точка.
+
+using System.Runtime.Intrinsics.X86;
+
+Console.WriteLine("Введите значение точки X: ");
+int X = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение точки Y: ");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+if (X > 0 && Y > 0)
+{
+    Console.WriteLine("1");
+}
+else if (X < 0 && Y > 0)
+{
+    Console.WriteLine("2");
+}
+else if (X < 0 && Y < 0)
+{
+    Console.WriteLine("3");
+}
+else if (X > 0 && Y < 0)
+{
+    Console.WriteLine("4");
+}
+else 
+{
+    Console.WriteLine("X ≠ 0 и Y ≠ 0");
+}
