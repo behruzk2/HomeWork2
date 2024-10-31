9893d86d171fb7db48819482efa70dd6f1e858d5
Behruz <behruzk2002@mail.ru>
2024-10-31 13:19:31 -0400

    Completed task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход целое число из отрезка
+// [10, 99] и показывает наибольшую цифру числа
+
+Console.WriteLine("Введите любое число от 10 до 99: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 10 && num <=99)
+{
+    int firstDigit = num / 10;
+    int lastDigit = num % 10;
+    if (firstDigit >= lastDigit)
+    {
+        Console.WriteLine(firstDigit);
+    }
+    else
+    {
+        Console.WriteLine(lastDigit);
+    }
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
+}
